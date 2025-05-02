b23c5c93e4b942e72ba3f9af4ca7592397f37a95
Gabriel Prata <ghprata7245@gmail.com>
2025-05-02 20:18:23 -0300

    Adicionadas classes de security

---
+++ src/main/java/com/Prata/usuario/infrastructure/entity/Usuario.java
@@ -0,0 +1,50 @@
+package com.Prata.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter // Lombok Gera Getter e Setter atutomático
+@Setter
+@AllArgsConstructor // Lombok cria Contrutor sem argumentos e com argumentos automático
+@NoArgsConstructor
+@Entity   // Aponta para o Spring que essa é uma tabela do banco de dados
+@Table(name = "usuario") // Caso nao coloque o nome da tabela por padrao ele coloca o nome da classe
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera Id automático
+    private Long id;
+    @Column(name = "nome", length = 100) // Cria coluna no banco de dados
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+
+    @OneToMany(cascade = CascadeType.ALL) // Um usuario para muitos ou OneToOne para um
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Endereco> enderecos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
